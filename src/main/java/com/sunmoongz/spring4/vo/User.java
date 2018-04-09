952e5130df5d95f71bf1b7e3f6e9a18bbad0632f
826104943@qq.com <youj0801@github>
2018-04-09 16:00:22 +0800

    提交

---
+++ src/main/java/com/sunmoongz/spring4/vo/User.java
@@ -0,0 +1,27 @@
+package com.sunmoongz.spring4.vo;
+
+/**
+ * Created by G05326 on 2018/4/9.
+ */
+public class User {
+
+    private String username;
+
+    private String pwd;
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPwd() {
+        return pwd;
+    }
+
+    public void setPwd(String pwd) {
+        this.pwd = pwd;
+    }
+}
